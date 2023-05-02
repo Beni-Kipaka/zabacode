de323b248bd3359aaf1e57101777bbf3cc35443c
Beni-Kipaka <benikipaka01@gmail.com>
2023-05-02 20:05:41 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// NAVBAR TOGGLE
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const elemArr = [navCloseBtn, overlay, navOpenBtn];
+
+for (let i = 0; i < elemArr.length; i++){
+    elemArr[i].addEventListener("click", function(){
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
+
+
+
+
+
+// NAVBAR & OVERLAY TOGGLE LORS D'UN CLICK SUR N'IMPORTE QUEL LIEN
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++){
+    navbarLinks[i].addEventListener("click", function(){
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
+
+
+// GO TOP BTN STARTS QUAND L'ECRAN VA SCROLER JUSQU'A 400PX
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY >= 400){
+        header.classList.add("active")
+        goTopBtn.classList.add("active")
+    } else{
+        header.classList.remove("active")
+        goTopBtn.classList.remove("active")
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// Focus Developpeurs 2023
